d5bd0f0134a3f66b2047960e81d287cd4dbf78b4
Willin Wang <willin@willin.org>
2017-09-04 11:57:01 +0800

    add async/await support

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ const _ = require('pipefn');
 const result = [1, 2, 3, 4, 5] |> _([].concat, [6, 7, 8, 9, 0]);
 
 console.log(result);
+
+
+(async () => {
+  const test = async t => t;
+  const result2 = 63 |> await test;
+  console.log(result2);
+})();
